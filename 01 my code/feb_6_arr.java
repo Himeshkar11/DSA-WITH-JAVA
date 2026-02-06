0513ec9052024c01b159119cc7595b03c39cc61a
Himeshkar11 <himeshkarm0111@gmail.com>
2026-02-07 00:43:42 +0530

    arrays part 1 completed

---
+++ 01 my code/feb_6_arr.java
@@ -0,0 +1,13 @@
+public class feb_6_arr {
+    public static void main(String[] args) {
+    // arrays in java 
+    // declaration syntax of an array 
+    // dtype [] arr_name = new dtype[size];
+    // the below is an array to store 5 integers
+    // int[] roll_no  = new int[5];
+    // System.out.println(roll_no[4]);
+    String[] arr = new String[5];
+    System.out.println(arr);
+    }
+
+}
